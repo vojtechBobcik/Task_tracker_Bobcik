75ae4d99b9c83571cf4cc7cb0188491d7ec15f7b
VojtechBobcik <xbobcik@mendelu.cz>
2026-04-11 12:34:51 +0200

    33_ Using Input decorator on avatar, name, imagePath

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,18 +9,18 @@ import { Component, Input, input, computed} from '@angular/core';
 })
 export class UserComponent {
 
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
   
 
-  // get imagePath(){
-  //   return `assets/users/${this.avatar}`;
-  // }
-  imagePath = computed(() => { 
-    return `assets/users/${this.avatar()}`;
-  })
+  get imagePath(){
+    return `assets/users/${this.avatar}`;
+  }
+  // imagePath = computed(() => { 
+  //   return `assets/users/${this.avatar()}`;
+  // })
 
   onSelectUser(){}
 }
